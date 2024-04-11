6b1f270686e2962d8a3fa9b0833ceab3a0083f77
Frederich Blessy <frederichblessy@gmail.com>
2024-04-11 12:01:06 +0700

    feature: add mark on edited row

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useReducer, useEffect } from "react";
+import { useState, useReducer, useEffect, useRef } from "react";
 
 import "./index.css";
 
@@ -7,6 +7,7 @@ import {
   getCoreRowModel,
   flexRender,
 } from "@tanstack/react-table";
+import { useCallback } from "react";
 
 const getTextWidth = (text) => {
   const canvas = document.createElement("canvas");
@@ -124,14 +125,14 @@ const defaultColumns = col.map((item) => {
 });
 
 const defaultColumn = {
-  cell: ({ getValue, row: { index }, column: { id }, table }) => {
+  cell: ({ getValue, row, column: { id }, table }) => {
     const initialValue = getValue();
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [value, setValue] = useState(initialValue);
 
     // When the input is blurred, we'll call our table meta's updateData function
     const onBlur = () => {
-      table.options.meta?.updateData(index, id, value);
+      table.options.meta?.updateData(row.index, id, value);
     };
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -139,21 +140,61 @@ const defaultColumn = {
       setValue(initialValue);
     }, [initialValue]);
 
-    return (
-      <input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onBlur={onBlur}
-      />
-    );
+    if (row.getIsSelected()) {
+      return (
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onDoubleClick={(e) => e.target.select()}
+          onBlur={onBlur}
+        />
+      );
+    }
+
+    return <p>{value}</p>;
   },
+};
+
+const useSkipper = () => {
+  const shouldSkipRef = useRef(true);
+  const shouldSkip = shouldSkipRef.current;
+
+  // Wrap a function with this to skip a pagination reset temporarily
+  const skip = useCallback(() => {
+    shouldSkipRef.current = false;
+  }, []);
+
+  useEffect(() => {
+    shouldSkipRef.current = true;
+  });
+
+  return [shouldSkip, skip];
+};
+
+const isIdenticalObject = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false; // Different number of properties
+  }
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false; // Different property values
+    }
+  }
+
+  return true; // Objects have the same properties and values
 };
 
 function App() {
   const [columns] = useState(() => [...defaultColumns]);
   const [data, setData] = useState(() => [...users]);
+  const [rowSelection, setRowSelection] = useState({});
 
   const rerender = useReducer(() => ({}), {})[1];
+  const [autoResetPageIndex, skipAutoResetPageIndex] = useSkipper();
 
   const table = useReactTable({
     data,
@@ -162,6 +203,41 @@ function App() {
     defaultColumn,
     getCoreRowModel: getCoreRowModel(),
     enableRowSelection: true,
+    enableMultiRowSelection: false,
+    onRowSelectionChange: setRowSelection, //hoist up the row selection state to your own scope
+    state: {
+      rowSelection, //pass the row selection state back to the table instance
+    },
+    autoResetPageIndex,
+    meta: {
+      updateData: (rowIndex, columnId, value) => {
+        // Skip page index reset until after next rerender
+        skipAutoResetPageIndex();
+        setData((old) =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              // Skip updating the row if the value is the same
+              if (row[columnId] === value) return row;
+
+              const newRow = {
+                ...old[rowIndex],
+                [columnId]: value,
+              };
+
+              if (users[rowIndex][columnId] !== value) {
+                newRow.edited = true;
+              } else {
+                newRow.edited = false;
+              }
+
+              return newRow;
+            }
+
+            return row;
+          })
+        );
+      },
+    },
   });
 
   return (
@@ -191,7 +267,6 @@ function App() {
                         },
                       }}
                     >
-                      {console.log("size nih", header.getSize())}
                       {header.isPlaceholder
                         ? null
                         : flexRender(
@@ -219,34 +294,62 @@ function App() {
               className="tbody"
               style={{ width: "100%", overflowX: "scroll" }}
             >
-              {table.getRowModel().rows.map((row) => (
-                <div
-                  key={row.id}
-                  className={`tr ${
-                    row.index % 2 === 0 ? "even-row" : "odd-row"
-                  }`}
-                >
-                  {row.getVisibleCells().map((cell) => {
-                    return (
-                      <div
-                        className="td"
-                        key={cell.id}
-                        {...{
-                          key: cell.id,
-                          style: {
-                            width: cell.column.getSize(),
-                          },
-                        }}
-                      >
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </div>
-                    );
-                  })}
-                </div>
-              ))}
+              {table.getRowModel().rows.map((row) => {
+                const currentRow = row.getVisibleCells().reduce((acc, cell) => {
+                  return {
+                    ...acc,
+                    [cell.column.id]: cell.getContext().getValue(),
+                  };
+                }, {});
+
+                const originalRow = users[row.index];
+
+                let editedRow = false;
+                if (!isIdenticalObject(currentRow, originalRow)) {
+                  editedRow = true;
+                }
+
+                return (
+                  <div
+                    key={row.id}
+                    className={`tr ${
+                      !row.getIsSelected()
+                        ? !editedRow
+                          ? row.index % 2 === 0
+                            ? "even-row"
+                            : "odd-row"
+                          : "edited-row"
+                        : "selected-row"
+                    }`}
+                    onClick={() => {
+                      if (!row.getIsSelected()) {
+                        row.toggleSelected();
+                        return;
+                      }
+                    }}
+                  >
+                    {row.getVisibleCells().map((cell) => {
+                      return (
+                        <div
+                          className="td"
+                          key={cell.id}
+                          {...{
+                            key: cell.id,
+                            style: {
+                              width: cell.column.getSize(),
+                            },
+                          }}
+                        >
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
